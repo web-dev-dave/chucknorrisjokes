af02628a96edd59b635f892524b0e2f8bafb8166
David Steel <65894147+web-dev-dave@users.noreply.github.com>
2021-01-13 21:32:24 +1300

    consuming external api

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-document.getElementById()
+document.querySelector('.get-jokes').addEventListener('click', getJokes)
+
+function getJokes(e) {
+  // console.log('This is a joke')
+  const number = document.getElementById('number').value
+
+  // console.log(number)
+  const xhr = new XMLHttpRequest
+
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true)
+
+  xhr.onload = function() {
+    
+    if (xhr.status === 200) {
+      // console.log(this.responseText)
+
+      let output = ''
+
+      const response = JSON.parse(this.responseText)
+      // console.log(response)
+
+      if (response.type === 'success') {
+        response.value.forEach(function(joke){
+          output += `<li>${joke.joke}</li>`
+        })
+
+        console.log(output)
+        document.querySelector('.jokes').innerHTML = output
+      }
+
+      
+    } else {
+      document.querySelector('.jokes').innerHTML = 'Something went wrong'
+    }
+  }
+
+  xhr.send()
+
+  e.preventDefault()
+}
